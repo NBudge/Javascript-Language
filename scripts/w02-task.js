98e73c40e0688aaf9e46ccfc739dc3542c11f9c4
Nathanael Budge <nathanaelcbudge@gmail.com>
2023-09-18 09:34:19 -0600

    added w-02 files

---
+++ scripts/w02-task.js
@@ -0,0 +1,35 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Nathanael Budge';
+let currentYear = int(2030);
+let profilePicture = "images/WIN_20230601_14_52_02_Pro.jpg"
+
+
+
+/* Step 3 - Element Variables */
+let nameElement = document.getElementById('name');
+let foodElement = document.getElementById('food');
+let yearElement = document.getElementById('#year');
+var img1 = document.createElement('img');
+img1.src = "images\WIN_20230601_14_52_02_Pro.jpg";
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+//yearElement.textContent = `${currentYear}`;
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
